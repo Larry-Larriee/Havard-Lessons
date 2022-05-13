0ce2029d79415d8cc695ad8b5d884a06545ddbbc
Larry Le <lle114@student.hbuhsd.edu>
2022-05-12 18:36:53 -0700

    Add Instructions

--- UserInput.py
+++ UserInput.py
@@ -1,3 +1,9 @@
+'''
+Ask the user for his or her favorite number and double it.
+For example: if my favorite number is 7, output an integer value of 14
+'''
+
+
 # VARIABLES ----------------------------------------------------------------------------------------------------
 
 num = input("What is you favorite number: ")
